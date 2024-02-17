b2f5ea70dabfb3f3abfef4de475736865a0a6e5d
Roy <roychav99@gmail.com>
2024-02-17 12:16:21 -0600

    Sumador1bit

---
+++ Sumador4bits/Sumador4bits.vhd
@@ -0,0 +1,17 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+ 
+entity Sumador4bits is
+ Port ( A : in STD_LOGIC;
+ B : in STD_LOGIC;
+ CarryIn : in STD_LOGIC;
+ Sum : out STD_LOGIC;
+ CarryOut : out STD_LOGIC);
+end Sumador4bits;
+ 
+architecture gate of Sumador4bits is
+ 
+begin
+ 
+ 
+end gate;
