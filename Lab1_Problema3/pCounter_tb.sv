edf28ac8f5fb27f2fbb8008b01a7f7ebb95a36ac
Jose <jcur1802@gmail.com>
2024-02-18 17:41:02 -0600

    Testbench contador 4 y 6 bits
    se agrego las pruebas para los contadores de 4 y 6 bits

--- Lab1_Problema3/pCounter_tb.sv
+++ Lab1_Problema3/pCounter_tb.sv
@@ -4,11 +4,25 @@ module pCounter_tb();
     
     logic clk = 0, r;
     logic [1:0] out2;
+	 logic [3:0] out4;
+	 logic [5:0] out6;
     
-    pCounter #(2) counter (
+    pCounter #(2) counter_2bit (
         .clk(clk),
         .r(r),
         .out(out2)
+    );
+	 
+	 pCounter #(4) counter_4bit (
+        .clk(clk),
+        .r(r),
+        .out(out4)
+    );
+	 
+	 pCounter #(6) counter_6bit (
+        .clk(clk),
+        .r(r),
+        .out(out6)
     );
 
     initial begin
