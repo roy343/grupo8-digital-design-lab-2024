a85aa23483fa05f2d77113c00f660e2ea5c15447
Andrés <andresmolina@estudiantec.cr>
2024-02-19 14:36:37 -0600

    Correcciones
    Se realizaron correcciones al archivo Decoder.sv para que se realice un comportamiento adecuado

--- Decoder.sv
+++ Decoder.sv
@@ -2,15 +2,15 @@ module Decoder(input logic a, b, c, d, output logic t, u, v, w, x, y, z);
 
 assign t = (b|d) & (!b|!d) & !c & !a;
 
-assign u = b & d;
+assign u = (c|d) & (!c|!d) & b;
 
-assign v = c & d & !b;
+assign v = c & !d & !b;
 
-assign w = (b|d) & (!c|!d) & (!b|!d) & !a;
+assign w = (!b|c|!d) & (b|d) & (b|!c) & (!c|d) & !a;
 
-assign x = (b|c|d) & (!c|!d);
+assign x = (b|d) & (!c|d);
 
-assign y = (c|d) & (!b|!d) & !a;
+assign y = (c|d) & (!b|c) & (!b|d) & !a;
 
 assign z = (b|!c) & (!b|c) & (!b|!d) & !a;
 
