dbca038690dfe183c02b93216d0c0cf35542d893
Roy <roychav99@gmail.com>
2024-02-29 23:21:49 -0600

    ALU base con operadores

--- Laboratorio2/ALU.sv
+++ Laboratorio2/ALU.sv
@@ -1,16 +1,36 @@
-module ALU (input [7:0] A,B,  // ALU 8-bit Inputs                 
-           input [3:0] OpCode,// ALU Selection
-           output [7:0] Out, // ALU 8-bit Output
-           output Flag);
-			  
-	reg [7:0] Result;
-	wire [8:0] tmp;
-	assign Out = ALU_Result;
-	
-	begin
-        case(ALU_Sel)
-		  
+module ALU(
+           input [3:0] A,B,                   
+           input [3:0] OpCode,
+           output [3:0] Out, 
+           output Flag 
+    );
+    reg [3:0] Result;
+	 assign Out = Result;
+	 always @(*)
+	 
 
-        default: ALU_Result = A + B ; 
+    begin
+        case(OpCode)
+        4'b0000: // SUMA (CAMBIAR)
+           Result = A + B ; 
+        4'b0001: // RESTA (CAMBIAR)
+           Result = A - B ;
+        4'b0010: // MULTIPLICACION (CAMBIAR)
+           Result = A * B; 
+        4'b0011: // DIVISION (CAMBIAR)
+           Result = A/B;
+        4'b0100: // SHIFT LEFT
+           Result = A<<1;
+        4'b0101: // SHIFT RIGHT
+           Result = A>>1;
+        4'b1000: //  AND 
+           Result = A & B;
+        4'b1001: //  OR
+           Result = A | B;
+		   4'b1010: //  XOR
+           Result = A ^ B;
+          default: Result = A & B ; 
         endcase
     end
+
+endmodule
